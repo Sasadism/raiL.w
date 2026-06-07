b14217125b3cc0ac3f6461d84f3a9589b6040d32
Sasadism <104597434+Sasadism@users.noreply.github.com>
2026-06-07 09:27:45 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  const target = "https://178.105.179.72:443"  //
-  const newUrl = target + url.pathname + url.search
-  const modifiedHeaders = new Headers(request.headers)
-  modifiedHeaders.set("x-host", "x-host":"netlify.parsashonam.sbs:444") //
-  const modifiedRequest = new Request(newUrl, {
-    method: request.method,
-    headers: modifiedHeaders,
-    body: request.body
-  })
-  return fetch(modifiedRequest)
+export default {
+  async fetch(request) {
+    const url = new URL(request.url)
+    const target = "https://178.105.179.72:443"
+    const newUrl = target + url.pathname + url.search
+    const modifiedHeaders = new Headers(request.headers)
+    modifiedHeaders.set("x-host", "netlify.parsashonam.sbs:444")
+    const modifiedRequest = new Request(newUrl, {
+      method: request.method,
+      headers: modifiedHeaders,
+      body: request.body
+    })
+    return fetch(modifiedRequest)
+  }
 }
